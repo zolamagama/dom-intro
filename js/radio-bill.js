235957a4e2b00a983185291ec699a04f0633dd75
Zola Magama <zolamagama1985@gmail.com>
2020-06-16 18:21:01 +0200

    add these changes

--- js/radio-bill.js
+++ js/radio-bill.js
@@ -1,11 +1,7 @@
 // get a reference to the sms or call radio buttons
-
 //get a reference to the add button
-
 //create a variable that will keep track of the total bill
-
 //add an event listener for when the add button is pressed
-
 //in the event listener get the value from the billItemTypeRadio radio buttons
 // * add the appropriate value to the running total
 // * add nothing for invalid values that is not 'call' or 'sms'.
@@ -13,41 +9,37 @@
 const radioBillBtnElem = document.querySelector(".radioBillAddBtn")
 const callTotalCostElem = document.querySelector(".callTotalTwo")
 const smsTotalCostElem = document.querySelector(".smsTotalTwo")
-const totalElem3 = document.querySelector(".totalTwo")
+const totalElem = document.querySelector(".totalTwo")
 var callTotal2 = 0;
 var smsTotal2 = 0;
-var totalCost = 0;
+//var totalCost = 0;
 function clicked() {
     var checkedRadioBtn = document.querySelector("input[name='billItemType']:checked");
-    if (checkedRadioBtn) {
         var x = checkedRadioBtn.value
         // billItemType will be 'call' or 'sms'
         smsTotalCostElem.innerHTML = smsTotal2.toFixed(2);
         callTotalCostElem.innerHTML = callTotal2.toFixed(2);
-        styleCost()
-        totalElem3.innerHTML = tcostBill(x);
+        var totalCost = smsTotal2 + callTotal2;
+       // styleCost(totalCost)
+        totalElem.innerHTML = totalCost.toFixed(2);
+    
 
-    }
-}
-function tcostBill(y) {
-    if (y === 'sms') {
+    if (x === 'sms') {
         smsTotal2 += 0.75;
     }
-    else if (y === 'call') {
+    else if (x === 'call') {
         callTotal2 += 2.75;
     }
-    var totalCost = (smsTotal2 + callTotal2).toFixed(2)
-    return totalCost
-}
-function styleCost() {
-    const bill = Number(totalCost);
-    totalElem3.classList.remove("warning");
-    totalElem3.classList.remove("danger")
-    if (bill >= 50) {
-        totalElem3.classList.add("danger");
+    
+   // return totalCost
+
+  //  totalElem.classList.remove("warning");
+  //  totalElem.classList.remove("danger")
+    if (totalCost >= 50) {
+        totalElem.classList.add("danger");
     }
-    else if (bill >= 30 && bill < 50) {
-        totalElem3.classList.add("warning");
+    else if (totalCost >= 30 && totalCost < 50) {
+        totalElem.classList.add("warning");
     }
 }
-radioBillBtnElem.addEventListener('click', clicked)
+radioBillBtnElem.addEventListener("click", clicked)
